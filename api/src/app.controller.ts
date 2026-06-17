6e75574a7eb05d05b66fb927dc4b143dd2495fda
Adriano <>
2026-06-17 08:17:37 -0300

    feat: add public access decorator to the index route in AppController

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import pkg from '../package.json';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor() {}
 
+  @Public()
   @Get()
   index() {
     return {
